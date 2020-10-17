fc27e4d4c2ad9ec80bfd13908f2bb2aa6010a096
lmiro <lynnmiro65@gmail.com>
2020-10-17 18:20:09 -0400

    Add the business service for topic

--- src/main/java/org/learning/topic/TopicController.java
+++ src/main/java/org/learning/topic/TopicController.java
@@ -1,20 +1,23 @@
 package org.learning.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
+
+    private TopicService topicService;
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "JavaScript", "JavaScript Description")
-
-        );
+        return topicService.getAllTopics();
     }
 }
