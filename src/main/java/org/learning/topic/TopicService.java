6932858f36941679a1314c5784c23b1e3f392fc0
lmiro <lynnmiro65@gmail.com>
2020-10-18 20:31:38 -0400

    Adding add, update, delete, findone and findall using TopicRepository and JPA Data Spring

--- src/main/java/org/learning/topic/TopicService.java
+++ src/main/java/org/learning/topic/TopicService.java
@@ -1,5 +1,6 @@
 package org.learning.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,48 +10,36 @@ import java.util.List;
 @Service
 public class TopicService {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javascript", "JavaScript", "JavaScript Description")));
 
     public List<Topic> getAllTopics(){
+        List<Topic> topics = new ArrayList<>();
+        Iterable<Topic> itr = topicRepository.findAll();
+        for (Topic topic: itr){
+            topics.add(topic);
+        }
         return topics;
     }
 
     public Topic getTopic(String id){
-        //return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-        for (Topic topic: topics){
-            String temp = topic.getId();
-            if (temp.equals(id)){
-                return topic;
-            }
-        }
-
-        return null;
+        return topicRepository.findOne(id);
     }
 
     public void addTopic(Topic topic){
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(Topic topic, String id){
-        for(int i = 0; i < topics.size(); i++){
-            Topic topic1 = topics.get(i);
-            if(topic1.getId().equals(id)){
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id){
-        for(int i = 0; i < topics.size(); i++){
-            Topic topic1 = topics.get(i);
-            if(topic1.getId().equals(id)){
-                topics.remove(topic1);
-                return;
-            }
-        }
+        topicRepository.delete(id);
     }
 }
